df1595317fd0b08679957bd43bab85371ee157ee
JanChw <1134806787@qq.com>
2017-06-09 12:18:51 +0800

    完成日志记录

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const cookie = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const api = require('./api/v1/api');
 const tools = require('./middlewares/tools');
 const isObjectId = require('./middlewares/isObjectId');
+const fs = require('fs');
+const util = require('util');
 
 const app = express();
+
+let accessLogStream = fs.createWriteStream('./access.log', {flags: 'a'});
+app.use(morgan('short', {stream: accessLogStream}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -24,11 +31,21 @@ app.use(tools);
 
 app.use(api);
 
-app.use((err,req,res,next)=>{
-  console.error(err.stack);
+app.use(async (err,req,res,next)=>{
+  // console.error(err.stack);
+  
+let writeStream = fs.createWriteStream('./error.log',{flags:'a'});
+  // let isWriting = writeStream.write(err.stack);
+//  await writeStream.end(err.stack);
+// writeStream.cork();
+writeStream.write(err.message);
+writeStream.write('\n');
+writeStream.write('===========================================\n');
+// writeStream.uncork();
+writeStream.end();
+
   res.status(500).send('something broke!');
 })
 app.listen(3000,()=>{
-  console.log(app.param);
   console.log('server starting on port 3000')
 });
--- backend/middlewares/isObjectId.js
+++ backend/middlewares/isObjectId.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 module.exports = (req,res,next,id) => {
 
   if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.json({msg:'此id不存在！'});
+     next(new Error('此id不存在！'));
+     return;
+    // return res.json({msg:'此id不存在！'});
   }
-  next()
+  next();
 }
